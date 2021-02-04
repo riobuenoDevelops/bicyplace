5ffcd19b0622900e0ca98cbb7a0cfd01d39ccac7
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-02-04 07:58:27 -0400

    Setup: Removing unnecessary files

--- bicyclient/src/App.js
+++ bicyclient/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@chakra-ui/react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+//Pages
+import LoginPage from "./layouts/LoginPage";
+import SignUpPage from "./layouts/SignUpPage";
+import NavbarLayoutPage from "./layouts/NavbarLayoutPage";
+
+//Services
+import KuzzleService from "./services/kuzzle";
+
+async function connect() {
+  await KuzzleService.connect();
+  console.log("Kuzzle Connected");
+}
 
 function App() {
+  connect();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box w="100vw" h="100vh" overflowX="hidden">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <LoginPage kuzzleService={KuzzleService} />
+          </Route>
+          <Route path="/signup">
+            <SignUpPage kuzzleService={KuzzleService} />
+          </Route>
+          <NavbarLayoutPage>
+            <Route path="/">
+              <Box></Box>
+            </Route>
+          </NavbarLayoutPage>
+        </Switch>
+      </BrowserRouter>
+    </Box>
   );
 }
 
