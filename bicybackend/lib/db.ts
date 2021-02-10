8159a4add6965e3b14714f00e07a49bf8b8f54aa
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-02-10 05:19:40 -0400

    Feature: collection creation setup

--- bicybackend/lib/db.ts
+++ bicybackend/lib/db.ts
@@ -16,6 +16,24 @@ export default async function createDatabase(app) {
       await app.sdk.collection.create("bicyplace", "products");
       app.log.info("Products collection created");
     }
+
+    const notificationExists = await app.sdk.collection.exists(
+      "bicyplace",
+      "notifications"
+    );
+    if (!notificationExists) {
+      await app.sdk.collection.create("bicyplace", "notifications");
+      app.log.info("Notification collection created");
+    }
+
+    const subscriptionExists = await app.sdk.collection.exists(
+      "bicyplace",
+      "subscriptions"
+    );
+    if (!subscriptionExists) {
+      await app.sdk.collection.create("bicyplace", "subscriptions");
+      app.log.info("Subscriptions collection created");
+    }
   } catch (err) {
     console.error(err.message);
   }
